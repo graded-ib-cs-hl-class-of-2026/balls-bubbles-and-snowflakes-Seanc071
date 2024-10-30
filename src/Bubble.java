88a512d475edbd58e4ad44d17006169c6d53f76a
Sean <No>
2024-10-30 13:56:18 -0300

    level 5

--- src/Bubble.java
+++ src/Bubble.java
@@ -1,10 +1,41 @@
-/** THE ENTIRE CLASS IS INSIDE A COMMENT RIGHT NOW. TO ENABLE THE CLASS, DELETE THIS LINE AND THE LAST ONE.
 
 class Bubble {
 
     //MISSING MANY INSTANCE VARIABLES
+    private Sketch s;
+    /** the radius of the ball */
+    private float radius;
+    private float x;
+    private float y;
+    /** The number of pixels the ball moves right per frame */
+    private float xSpeed;
+    /** The number of pixels the ball moves down per frame */
+    private float ySpeed;
+    /** The color of the inside of the ball */
+    private int fillColor;
+    /** The color of the outside of the ball */
+    private int borderColor;
 
-    //MISSING CONSTRUCTORS
+/**constructor */
+    public Bubble(Sketch sketch) {
+        s = sketch;
+        radius = 20;
+        x = 100;
+        y = 100;
+        xSpeed = 2;
+        ySpeed = -1;
+        fillColor = s.color(255, 100, 100);
+        borderColor = s.color(0, 0, 0);
+
+    }
+    public Bubble(Sketch sketch, float radius, float x, float y, float xspeed, float yspeed) {
+        this.s = sketch;
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xspeed;
+        this.ySpeed = yspeed;
+    }
 
     // accessors for the radius, diameter, x, and y values 
     public float getRadius() {
@@ -28,6 +59,11 @@ class Bubble {
         s.fill(fillColor);
         s.circle(x, y, radius*2);
     }
+    public void setColors(int fill, int border) {
+        borderColor = border;
+        fillColor = fill;
+    }
+
 
     public void move() {
         x = x + xSpeed;
@@ -44,4 +80,4 @@ class Bubble {
     }
 
 }
- **/
+ 
--- src/Sketch.java
+++ src/Sketch.java
@@ -4,6 +4,22 @@ public class Sketch extends PApplet {
 
     /** Represents one ball */
     private Ball ball1;
+    /**made total 4 balls */
+    private Ball ball2;
+    private Ball ball3;
+    private Ball ball4;
+/**create 4 bubbles */
+    private Bubble bubble1;
+    private Bubble bubble2;
+    private Bubble bubble3;
+    private Bubble bubble4;
+/**create 4 snowflakes */
+    private Snowflake snowflake1;
+    private Snowflake snowflake2;
+    private Snowflake snowflake3;
+    private Snowflake snowflake4;
+
+
 
     /**
      * This method can only be used to change the window size. It runs before the
@@ -18,7 +34,37 @@ public class Sketch extends PApplet {
      * this to initialize the sketch.
      */
     public void setup() {
-        ball1 = new Ball(this);
+        /**where the ball is constructed, radius, x position, y postion, x speed, y speed */
+        ball1 = new Ball(this, 19, 55, 11, -13, 15);
+        /**Setting the color of the ball (red, green, blue) */
+        ball1.setColors(color(255,0,0),color(0));
+        ball2 = new Ball(this, 25, 29, 29, 7, -8);
+        ball2.setColors(color(255,125,0),color(0));
+        ball3 = new Ball(this, 40, 112, 40, -3, 5);
+        ball3.setColors(color(255,255,0),color(0));
+        ball4 = new Ball(this, 50, 222, 61, 3, -1);
+        ball4.setColors(color(0,255,0),color(0));
+
+        /**where the bubble is constructed, radius, x position, y postion, x speed, y speed */
+        bubble1 = new Bubble(this, 45, 6,3, -4,-8);
+        bubble1.setColors(256,0);
+        bubble2 = new Bubble(this, 25, 6,3, -1,-1);
+        bubble2.setColors(256,0);
+        bubble3 = new Bubble(this, 10, 6,3, -2,-4);
+        bubble3.setColors(256,0);
+        bubble4 = new Bubble(this, 30, 6,3, 1,-2);
+        bubble4.setColors(256,0);
+
+        /**where the snowflakes is constructed, radius, x position, y postion, x speed, y speed */
+        snowflake1 = new Snowflake(this, 30, 6,5, -1,2);
+        snowflake1.setColors(0,0);
+        snowflake2 = new Snowflake(this, 45, 13,3, 0,5);
+        snowflake2.setColors(0,0);
+        snowflake3 = new Snowflake(this, 50, 3,1, -2,3);
+        snowflake3.setColors(0,0);
+        snowflake4 = new Snowflake(this, 25, 9,9, 2,7);
+        snowflake4.setColors(0,0);
+        
     }
 
     /**
@@ -27,9 +73,49 @@ public class Sketch extends PApplet {
      * movement.
      */
     public void draw() {
+        /**background size  */
         background(180, 180, 255);
+        
         ball1.draw();
         ball1.move();
+
+        ball2.draw();
+        ball2.move();
+
+        ball3.draw();
+        ball3.move();
+
+        ball4.draw();
+        ball4.move();
+
+
+
+        bubble1.draw();
+        bubble1.move();
+        
+        bubble2.draw();
+        bubble2.move();
+
+        bubble3.draw();
+        bubble3.move();
+
+        bubble4.draw();
+        bubble4.move();
+
+
+
+        snowflake1.draw();
+        snowflake1.move();
+
+        snowflake2.draw();
+        snowflake2.move();
+     
+        snowflake3.draw();
+        snowflake3.move();
+
+        snowflake4.draw();
+        snowflake4.move();
+       
     }
 
     /** All processing sketches have to use this main method. Don't touch this! */
